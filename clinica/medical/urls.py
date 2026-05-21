63b3f7d0651bc55321a0c713ebc60d35544a9ff1
otaviosbampato <otaviosbampato@gmail.com>
2026-05-21 11:27:38 -0300

    chore: remove "registro" functionality

--- clinica/medical/urls.py
+++ clinica/medical/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path("", views.login_view, name="home"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
-    path("registro/", views.registro_view, name="registro"),
     path("dashboard/", views.dashboard_view, name="dashboard"),
 ]
